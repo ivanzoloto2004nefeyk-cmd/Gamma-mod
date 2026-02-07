d93fa18c4c800e58af3cc672557a587c165c74a6
ivanzoloto2004nefeyk-cmd <ivanzoloto2004nefeyk@gmail.com>
2026-02-07 22:51:12 +0200

    Update MainMod.java

--- src/main/java/com/example/gammamod/MainMod.java
+++ src/main/java/com/example/gammamod/MainMod.java
@@ -1,1 +1,28 @@
+package com.example.gammamod;
 
+import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
+import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
+import net.minecraft.client.option.KeyBinding;
+import net.minecraft.client.util.InputUtil;
+import org.lwjgl.glfw.GLFW;
+
+public class MainMod implements ClientModInitializer {
+    private static KeyBinding openGammaGui;
+
+    @Override
+    public void onInitializeClient() {
+        openGammaGui = KeyBindingHelper.registerKeyBinding(new KeyBinding(
+                "key.gammamod.open_gui",
+                InputUtil.Type.KEYSYM,
+                GLFW.GLFW_KEY_G,
+                "key.categories.misc"
+        ));
+
+        ClientTickEvents.END_CLIENT_TICK.register(client -> {
+            while(openGammaGui.wasPressed()) {
+                client.setScreen(new GammaScreen());
+            }
+        });
+    }
+}
